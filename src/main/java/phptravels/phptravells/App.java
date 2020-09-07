a0758659efaeb0cb176bc460b23b0d0fe07556a8
kasuravikasu <kasu_ravikasu@hotmail.com>
2020-09-07 14:53:27 +0530

    Initial Project commit - Created New Maven Project

---
+++ src/main/java/phptravels/phptravells/App.java
@@ -0,0 +1,13 @@
+package phptravels.phptravells;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
